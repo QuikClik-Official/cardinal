be0f3c9bdf5692321c6fb7608a9c6ee9dfbb173c
QuikClik-Official <officialgrillmaster@gmail.com>
2026-06-25 14:41:19 +0100

    Update browserNames.cxx

--- branding/browserNames.cxx
+++ branding/browserNames.cxx
@@ -2,3 +2,4 @@ string browserName = "QuikClik Cardinal";
 string longName = "QuikClik Cardinal Browser";
 string noBrandName = "Cardinal";
 string noBrandNameLong = "Cardinal Browser";
+string brandName = "QuikClik";

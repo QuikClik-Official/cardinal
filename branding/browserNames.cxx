1495c6d6798c5a54adbd0d3319d2c70984f790b5
QuikClik-Official <officialgrillmaster@gmail.com>
2026-06-28 15:23:23 +0100

    it's in JavaScript now (like the rest of the codebase soon will be)

--- branding/browserNames.cxx
+++
@@ -1,5 +0,0 @@
-string browserName = "QuikClik Cardinal";
-string longName = "QuikClik Cardinal Browser";
-string noBrandName = "Cardinal";
-string noBrandNameLong = "Cardinal Browser";
-string brandName = "QuikClik";
---
+++ branding/browserNames.js
@@ -0,0 +1,5 @@
+const browserName = "QuikClik Cardinal";
+const longName = "QuikClik Cardinal Browser";
+const noBrandName = "Cardinal";
+const noBrandNameLong = "Cardinal Browser";
+const brandName = "QuikClik";
